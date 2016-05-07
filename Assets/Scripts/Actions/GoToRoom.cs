7072668501c5b48e67902f09308ca6e9ff80439b
JimmehG <j.gronowski@gmail.com>
2016-05-07 16:40:46 +1000

    Prefabs, Scripts, Sprites, Getting up animation

---
+++ Assets/Scripts/Actions/GoToRoom.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToRoom : Action {
+    public GameObject room;
+
+    public override void Perform()
+    {
+        goRoom();
+    }
+
+    public void goRoom()
+    {
+        Instantiate(room);
+        Destroy(transform.parent.gameObject);
+    }
+
+}
