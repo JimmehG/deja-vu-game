18b78e7d923f645804afa6cc461d285e95d22bdf
JimmehG <j.gronowski@gmail.com>
2016-05-08 17:27:09 +1000

    final build extra files

---
+++ Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollow : MonoBehaviour
+{
+    public GameObject follower;
+
+
+
+    void Update()
+    {
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse.x += 0.5f;
+        mouse.y -= 0.5f;
+        mouse.z = -9;
+        transform.position = mouse;
+    }
+}
