ea188c80eb292b88288117310b1f80818b4c2481
Albey Amakiir <albeyamakiir@gmail.com>
2016-05-08 11:23:16 +1000

    Added some day2 interactables

---
+++ Assets/Scripts/Actions/TriggerButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerButton : Action
+{
+
+	public GameController.Flag flagID;
+
+	public override void Perform()
+	{
+		if (!GameController.CheckFlag(flagID)) {
+			GameController.SetFlag(flagID);
+			//play noise
+		}
+		else
+		{
+			//play bad noise
+		}
+	}
+}
+
